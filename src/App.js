1ea7b44dd56cbc5b5e57ffcaf4cb8b19968ffceb
luvi-kh <nalsengom@daum.net>
2024-10-13 20:34:02 +0900

    MUI 기본 사용법

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+//import AcUnitIcon from '@mui/icons-material/AcUnit';
+import {AcUnit, Accessibility} from '@mui/icons-material';
+import styled from '@emotion/styled';
+import { Typography } from '@mui/material';
+//import AccessibilityIcon from '@mui/icons-material/Accessibility';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Button size="large" variant='text'>Hello World</Button>
+     <Button startIcon={<AcUnit />} fullWidth size="large" variant='contained'>Hello World</Button>
+     <Button endIcon={<Accessibility />} variant='outlined'>Hello World</Button>
+     <Button sx={{
+      backgroundColor: 'gray',
+      color: 'white',
+      margin: 10,
+      "&:hover": {
+        backgroundColor: 'black'
+      },
+      "&:disabled": {
+        backgroundColor: 'white',
+        color: 'black'
+      }
+     }}>Custom Style</Button>
+     <CustomButton>Custom Button Style</CustomButton>
+     <Typography variant='h1' gutterBottom >h1. Heading</Typography>
     </div>
   );
 }
+
+const CustomButton = styled(Button) ({
+  backgroundColor: 'gray',
+  color: 'white',
+  margin: 10,
+  "&:hover": {
+    backgroundColor: 'black'
+  },
+  "&:disabled": {
+    backgroundColor: 'white',
+    color: 'black'
+  }
+})
 
 export default App;
