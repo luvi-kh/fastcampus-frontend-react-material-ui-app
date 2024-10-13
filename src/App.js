716ba922beb68a442a123cd2bdbf69fb70029b6a
luvi-kh <nalsengom@daum.net>
2024-10-13 21:26:45 +0900

    Theme 사용하기

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Button from '@mui/material/Button';
 //import AcUnitIcon from '@mui/icons-material/AcUnit';
 import {AcUnit, Accessibility} from '@mui/icons-material';
 import styled from '@emotion/styled';
-import { Typography } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
+import { blue, green, red } from '@mui/material/colors';
+
 //import AccessibilityIcon from '@mui/icons-material/Accessibility';
 
 function App() {
@@ -26,9 +28,27 @@ function App() {
      }}>Custom Style</Button>
      <CustomButton>Custom Button Style</CustomButton>
      <Typography variant='h1' gutterBottom >h1. Heading</Typography>
+
+     <Button color="secondary">
+       Theme Button
+     </Button>
+     <CustomThemeButton>Custom Theme Button</CustomThemeButton>
     </div>
   );
 }
+
+const CustomThemeButton = styled(Button) (({theme}) => ({
+  backgroundColor: theme.palette.secondary.main,
+  color: 'white',
+  margin: 10,
+  "&:hover": {
+    backgroundColor: 'black'
+  },
+  "&:disabled": {
+    backgroundColor: 'white',
+    color: 'black'
+  }
+}))
 
 const CustomButton = styled(Button) ({
   backgroundColor: 'gray',
